d7d91a15573728d1a530ca3c0a2441967728636a
Airscript <francesco@airscript.it>
2023-01-21 16:55:16 +0100

    refactor: clean up server.ts file

--- server.ts
+++ server.ts
@@ -1,12 +1,9 @@
-// Importing: Loaders.
 import Application from "./loaders/application.loader";
 
-// Declaring Server Class.
 class Server extends Application {
   constructor() {
     super();
   }
 }
 
-// Starting up Server.
 new Server();
