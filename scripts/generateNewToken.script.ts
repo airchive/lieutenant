3b3d5ce86a0cb9387a00dcc311ebe5b6a58b1088
Airscript <francesco@airscript.it>
2023-01-21 16:42:37 +0100

    refactor: clean up in scripts folder

--- scripts/generateNewToken.script.ts
+++ scripts/generateNewToken.script.ts
@@ -1,23 +1,17 @@
-// Importing: Dependencies.
 import jwt from "jsonwebtoken";
-
-// Importing: Configs.
 import configs from "../.configs";
 
-// Declaring token payload interface.
 interface TokenPayload {
-  identifier: string;
+  email: string;
   database: string;
   username: string;
-  email: string;
+  identifier: string;
 }
 
-// Declaring token generation function.
 function generateNewToken(payload: TokenPayload): string {
   return jwt.sign(payload, configs.application.token.secret, {
     expiresIn: configs.application.token.expirationTime,
   });
 }
 
-// Exporting Script.
 export default generateNewToken;
