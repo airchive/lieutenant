65b7609c8d687ce00f097b7557b6830997881ad0
Airscript <francesco@airscript.it>
2023-01-21 16:42:49 +0100

    refactor: clean up configs file

--- .configs.ts
+++ .configs.ts
@@ -1,22 +1,19 @@
-// Importing: Dependencies.
 import dotenv from "dotenv";
-
-// Parsing .env content to process.env.
 dotenv.config();
 
-// Declaring global configs.
 export default {
   application: {
-    port: process.env.PORT || process.env.SERVER_PORT || 5000,
     environment: process.env.NODE_ENV || "development",
+    port: process.env.PORT || process.env.SERVER_PORT || 5000,
+
     token: {
-      secret: process.env.TOKEN_SECRET || "lieutenant",
       expirationTime: "3600000",
+      secret: process.env.TOKEN_SECRET || "lieutenant",
     },
   },
 
   database: {
-    url: "mongodb://mongo_black:27017,mongo_grey:27018,mongo_white:27019/",
     catalog: "catalog",
+    url: "mongodb://mongo_black:27017,mongo_grey:27018,mongo_white:27019/",
   },
 };
