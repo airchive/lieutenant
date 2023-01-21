01bd464d18fc602c986d6d24fb576430803db28a
Airscript <francesco@airscript.it>
2023-01-21 16:42:44 +0100

    refactor: clean up root router

--- src/v1/root.router.ts
+++ src/v1/root.router.ts
@@ -1,17 +1,10 @@
-// Importing: Dependencies.
 import express from "express";
 
-// Importing: Routers.
 import loginRouter from "./components/login/login.router";
 import itemsRouter from "./components/items/items.router";
 
-// Declaring variables.
 let v1 = express.Router();
-
-//#region Declaring routes.
 v1.use("/login", loginRouter);
 v1.use("/items", itemsRouter);
-//#endregion
 
-// Exporting Router.
 export default v1;
